69c247b57a529a163b1bc6b651e70b0cc9b78704
FilipAdrian <43296954+FilipAdrian@users.noreply.github.com>
2018-11-25 14:58:27 +0200

    Update LAB6.sql

--- Lab_6/LAB6.sql
+++ Lab_6/LAB6.sql
@@ -61,3 +61,121 @@ where Tip_Evaluare='Examen'
 		and Id_Student = any(select sef_grupa from grupe) 
 		and Nota !=10
 
+--Task no.5
+CREATE TABLE new_profesori
+(
+	Id_Profesor int primary key,
+	Nume_Profesor char (50),
+	Prenume_Profesor char (50),
+	Localitate char (60) DEFAULT ('mun. Chisinau'),
+	Adresa_1 char (60),
+	Adresa_2 char (60)
+	)
+INSERT INTO new_profesori (Id_Profesor,Nume_Profesor, Prenume_Profesor, Localitate,Adresa_1, Adresa_2)
+(SELECT Id_Profesor, Nume_Profesor, Prenume_Profesor, Adresa_Postala_Profesor, Adresa_Postala_Profesor, Adresa_Postala_Profesor
+from profesori)
+
+  -- Drop the function if it already exists
+  IF OBJECT_ID('INSTR', 'FN') IS NOT NULL
+	DROP FUNCTION INSTR
+  GO
+
+  CREATE FUNCTION INSTR (@str VARCHAR(8000), @substr VARCHAR(255), @start INT, @occurrence INT)
+  RETURNS INT
+  AS
+  BEGIN
+	DECLARE @found INT = @occurrence,
+			@pos INT = @start;
+ 
+	WHILE 1=1 
+	BEGIN
+		-- Find the next occurrence
+		SET @pos = CHARINDEX(@substr, @str, @pos);
+ 
+		-- Nothing found
+		IF @pos IS NULL OR @pos = 0
+			RETURN @pos;
+ 
+		-- The required occurrence found
+		IF @found = 1
+			BREAK;
+ 
+		-- Prepare to find another one occurrence
+		SET @found = @found - 1;
+		SET @pos = @pos + 1;
+	END
+ 
+	RETURN @pos;
+  END
+  GO
+
+INSERT INTO new_profesori(Id_Profesor, Nume_Profesor, Prenume_Profesor, Localitate, Adresa_1, Adresa_2)
+ SELECT Id_Profesor
+        , Nume_Profesor
+		, Prenume_Profesor
+		, CASE 
+		WHEN LEN(Adresa_Postala_Profesor)-LEN(REPLACE(Adresa_Postala_Profesor, ',', ''))=3 then  Substring(Adresa_Postala_Profesor,1, dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 2)-1)
+		WHEN LEN(Adresa_Postala_Profesor)-LEN(REPLACE(Adresa_Postala_Profesor, ',', '')) =2 THEN Substring(Adresa_Postala_Profesor,1, charindex(',',Adresa_Postala_Profesor)-1)
+		ELSE Adresa_Postala_Profesor
+		END as  localitate
+		, CASE 
+		WHEN LEN(Adresa_Postala_Profesor)-LEN(REPLACE(Adresa_Postala_Profesor, ',', ''))=3 then  Substring(Adresa_Postala_Profesor,dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 2)+1, (dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 3))-(dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 2))-1)
+		WHEN LEN(Adresa_Postala_Profesor)-LEN(REPLACE(Adresa_Postala_Profesor, ',', '')) =2 THEN Substring(Adresa_Postala_Profesor,dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 1)+1,(dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 2))-(dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 1))-1)
+		ELSE NULL
+		END as  Adresa_1
+		, CASE 
+		WHEN LEN(Adresa_Postala_Profesor)-LEN(REPLACE(Adresa_Postala_Profesor, ',', ''))=3 then  Substring(Adresa_Postala_Profesor,dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 3)+1, 5)
+		WHEN LEN(Adresa_Postala_Profesor)-LEN(REPLACE(Adresa_Postala_Profesor, ',', '')) =2 THEN Substring(Adresa_Postala_Profesor,dbo.INSTR(Adresa_Postala_Profesor, ',', 1, 2)+1, 5)
+		ELSE NULL
+		END as  Adresa_2
+		 FROM profesori
+		 select * from new_profesori
+select * from new_profesori
+
+
+--Task no.6
+create table orarul 
+( 
+	Id_Disciplina int,
+	Id_Profesor int,
+	Id_Grupa smallint default(1),
+	Zi char(10),
+	Ora Time,
+    Auditoriu int,
+	Bloc char(1) default('B') );
+	
+	
+	Insert into orarul (Id_Disciplina , Id_Profesor, Zi, Ora, Auditoriu)
+	values ( 107, 101, 'Luni','08:00', 202 ),
+			( 108, 101, 'Luni','11:30', 501 ),
+			( 109, 117, 'Luni','13:00', 501 );
+
+			select * from orarul
+
+--Task no.7
+INSERT INTO orarul (Id_Disciplina, Id_Profesor, Id_Grupa, Zi, Ora)
+values (
+	        (select Id_Disciplina from discipline where Disciplina = 'Structuri de date si algoritmi'),
+	        (select Id_Profesor from profesori where Nume_Profesor = 'Bivol' and Prenume_Profesor = 'Ion'),
+		    (select Id_Grupa from grupe where Cod_Grupa = 'INF171'), 
+		    'Luni', 
+		    '08:00')
+
+INSERT INTO orarul (Id_Disciplina, Id_Profesor, Id_Grupa, Zi, Ora)
+values (
+	        (select Id_Disciplina from discipline where Disciplina = 'Programe aplicative'),
+		    (select Id_Profesor from profesori where Nume_Profesor = 'Mircea' and Prenume_Profesor = 'Sorin'),
+		    (select Id_Grupa from grupe where Cod_Grupa = 'INF171'), 
+		    'Luni', 
+		    '11:30')
+
+INSERT INTO orarul (Id_Disciplina, Id_Profesor, Id_Grupa, Zi, Ora)
+values (
+	        (select Id_Disciplina from discipline where Disciplina = 'Baze de date'),
+		    (select Id_Profesor from profesori where Nume_Profesor = 'Micu' and Prenume_Profesor = 'Elena'),
+		    (select Id_Grupa from grupe where Cod_Grupa = 'INF171'), 
+		    'Luni', 
+		    '13:00')
+
+		select * from orarul
+
